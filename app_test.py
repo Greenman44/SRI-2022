d99994da65facfa1cbe04fea717deae2aa67b31b
Ernesto Alfonso <ernealfonso00@gmail.com>
2022-11-09 20:36:33 -0500

    Class structure of models

--- app_test.py
+++ app_test.py
@@ -1,10 +1,10 @@
 
-
-
+#Path Nouser D:\Universidad\Tercer año\Segundo semestre\SRI\documentos a leer\20news-bydate-train\alt.atheism\49960
+#Path Orejo D:\Cibernética\!!!!Tercer año\SRI\20news-bydate-train\alt.atheism\49960
 
 from pathlib import Path
 
-base = Path(r'D:\Cibernética\!!!!Tercer año\SRI\20news-bydate-train\alt.atheism\49960')
+base = Path(r'D:\Universidad\Tercer año\Segundo semestre\SRI\documentos a leer\20news-bydate-train\alt.atheism\49960')
 
 text = base.read_text()
 print(text.strip("\n"))
