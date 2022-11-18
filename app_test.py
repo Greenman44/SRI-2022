81181cb99792d6425dcd109cae35b54d69db5ccf
Abraham <gonzalezriveroabraham@gmail.com>
2022-11-17 19:35:51 -0500

    Update app_test.py
    docs

--- app_test.py
+++ app_test.py
@@ -1,23 +1,15 @@
 from library.datasets import Dataset
 from library.models import VectorialModel
 
+# aquí especificar el dataset con el que se va  a trabajar("Prueba", "cranfield", "20NewGroups")
+# alguno de los que están entre paréntesis
 docs = Dataset("Prueba")
 
 vectorial_Model = VectorialModel(docs)
 
+# aquí escribir la query
 rank = vectorial_Model.EvalQuery("I hate economic and stupid political")
 
 
 print(rank)
 print("*************************************************************")
-print(docs[1,"allan"])
-
-# basePath = getcwd() + "\\datasets\\Prueba_data.json"
-# df = pd.read_json(basePath, orient="body")
-
-# docs = df["body"]
-# docs = docs.map(lambda x: re.sub('[,\.!?()1234567890=;:$%&#]', '', x))
-# docs = docs.map(lambda x: re.sub(r'[^a-zA-Z0-9.\s]', ' ', x))  # Replace all non-alphanumeric characters with space
-# docs = docs.map(lambda x: re.sub(r'[^a-zA-Z0-9\s]', '', x))  # Remove all dots, e.g. U.S.A. becomes USA
-# docs = docs.map(lambda x: x.lower())
-# docs = docs.map(lambda x: x.strip())
