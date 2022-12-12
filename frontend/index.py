d3796b1b86deceba01b7ef26eb3da08aa53119c8
Ernesto Alfonso <ernealfonso00@gmail.com>
2022-12-11 20:18:58 -0500

    Front without doc visualization

--- frontend/index.py
+++ frontend/index.py
@@ -1,51 +1,68 @@
 from tkinter import *
+from tkinter.messagebox import showinfo
 from tkinter.ttk import * 
 from PIL import Image, ImageTk
+
 # TODO: Importar Models
 
 
-class Menu:
+class Menu():
     def __init__(self,root):
         self.window=root
         self.window.title("Menu")
         self.window.geometry("1920x1024")
         self.window.config(background="white")
 
-
-        # lupa= PhotoImage(file='OIP.png')
-        # lupa1=lupa.subsample(x=2,y=2)
-
-        Label(self.window, text = "Select the model to use in your search :",
+        Label(self.window, text = "Select the Dataset of documents to use in your search :",
           font = ("Times New Roman", 10),background = "white").grid(column = 0,
           row = 5, padx = 10, pady = 25)
-        n = StringVar()
-        modelchoosen = Combobox(self.window, width = 27, 
-                                    textvariable = n,
+        
+        self.selecteddocument=StringVar()
+        documentchoosen = Combobox(self.window, width = 27, 
+                                    textvariable = self.selecteddocument,
                                     font=("Times New Roman", 10),
-                                    background = "white")
-        # TODO: Lista de modelos
-        modelchoosen['values'] = (' LSI Model ', 
-                          ' Boolean Model ',
-                          ' Vectorial Model')
-  
-        modelchoosen.grid(column = 1, row = 5)
-        modelchoosen.current()
-        self.modelchoosen=modelchoosen.get()
+                                    background = "white",state="readonly",values=listD)
+        
+        documentchoosen.grid(column = 1, row = 5)
+
         Label(self.window,text = "                                             ",
                     background="white").grid(column=2,row=5)
 
+        Label(self.window, text = "Select the model to use in your search :",
+          font = ("Times New Roman", 10),background = "white").grid(column = 2,
+          row = 5, padx = 10, pady = 25)
+        self.selectedModel = StringVar()
+        modelchoosen = Combobox(self.window, width = 27, 
+                                    textvariable = self.selectedModel,
+                                    font=("Times New Roman", 10),
+                                    background = "white",state="readonly",values=listA)
+        
+        modelchoosen.bind("<<ComboboxSelected>>",self.MessageForModel)
+        modelchoosen.grid(column = 3, row = 5)
+        Label(self.window,text = "                                             ",
+                    background="white").grid(column=4,row=5)
+
         queryLabel=Label(self.window,text="Introduce la query:")
-        queryLabel.grid(row=5,column=4)
-        #NumSerie=StringVar()
-        queryUser=Entry(self.window)
-        queryUser.grid(row=5,column=3)
+        queryLabel.grid(row=5,column=7)
+        self.NumSerie=StringVar()
+        queryUser=Entry(self.window, textvariable=self.NumSerie)
+        queryUser.grid(row=5,column=10)
         self.queryUser=queryUser.get()
-        queryUser.bind("<Return>", self.Search)
+        queryUser.bind("<Return>", self.Beging)
 
 
-    def Search(self, event):
-        Label(self.window,text = "La query es: " + self.queryUser + " y el modelo es: " + self.modelchoosen).grid(column=1,row=10)
+    def Beging(self, event):
+        # TODO: Recoger los datos para procesarlos una vez procesados llamar al otro metodo para visualizarlos
+        Label(self.window,text = "La query es: " + self.NumSerie.get() + " y el modelo es: " + self.selectedModel.get()).grid(column=3,row=10)
+    
+    def MessageForModel(self,event):
+        if self.selectedModel.get()=="BooleanModel":
+            showinfo(title="Query info", message="For this model use boolean lenguage in query(Example: a & b, a | b, a and/or b, ~a & b)")
 
+
+
+listA=["BooleanModel","VectorialModel", "LSIModel"]
+listD=["Cranfield","20NewGroups"]
 
 
 
