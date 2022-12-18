55bc0716c1b739bac0369d81bfe1c137da24d4a1
Ernesto Alfonso <ernealfonso00@gmail.com>
2022-12-18 06:34:36 -0500

    Changes of Front

--- frontend/index.py
+++ frontend/index.py
@@ -4,6 +4,7 @@ from tkinter.ttk import *
 from backend import run
 import webbrowser
 from tkinter.font import Font, nametofont
+from tkinter.constants import DISABLED, NORMAL
 
 
 class Google:
@@ -13,6 +14,13 @@ class Google:
         self.window.geometry("1920x1024")
         self.window.config(background="white")
         self.doc=None
+
+        # frame2 = Frame(self.root)
+        # frame2.grid(row=0, column=0, sticky='ns')
+
+        # self.frame2=frame2
+        # # Add a canvas in that frame.
+        # 
 
         Label(
             self.window,
@@ -62,28 +70,72 @@ class Google:
             background="white",
         ).grid(column=4, row=5)
 
-        queryLabel = Label(self.window, text="Introduce query:")
+        queryLabel = Label(self.window, text="Introduce query:",background="white")
         queryLabel.grid(row=5, column=7)
         self.NumSerie = StringVar()
         queryUser = Entry(self.window, textvariable=self.NumSerie)
         queryUser.grid(row=5, column=10)
         self.queryUser = queryUser.get()
 
-        queryButton = Button(self.window, text= "Search", command = self.Begin)
-        queryButton.grid(row=5, column=11)
+        self.queryButton = Button(self.window, text= "Search", command = self.Begin,state=NORMAL)
+        self.queryButton.grid(row=5, column=11)
         self.linkrow=7
-        self.linkcol=-1
 
+        menubar=Menu(self.window)
+        self.window.config(menu=menubar)
+        
+        helpText="You have to complete all the camps in the window before press the search button. Every combobox contains all the elements you can use. If you selected the BooleanModel in combobox of Models, you need to use boolean lenguage, in other case use natural lenguage."
+        
+        
+        helpmenu = Menu(menubar,tearoff=0)
+        helpmenu.add_command(label="Basic Help", command = lambda m=helpText: self.OpenWindowHelp(m))
+        helpmenu.add_separator()
+        helpmenu.add_command(label="Quit", command=root.quit)
+
+        menubar.add_cascade(label="Help", menu=helpmenu)
+
+        
+        
+        stateButton = Button(self.window, text="Search Again", command = self.CloseFrame)
+        stateButton.grid(column=10,row=20, padx=20, pady=20)
+
+
+        
+
+    def on_configure(self,event):
+    # update scrollregion after starting 'mainloop'
+    # when all widgets are in canvas
+        self.canvas.configure(scrollregion=self.canvas.bbox('all'))
 
     def Begin(self):
+        self.lf= Frame(self.window,name='documents')
+        self.lf.grid(row=6,column=0,columnspan=10,sticky='ns')
+
+        self.canvas = Canvas(self.lf,background="white")
+        self.canvas.grid(column=0, row=0)
+        
+        
+        scrollbar = Scrollbar(
+                self.lf,
+                orient='vertical',
+                command=self.canvas.yview
+            )
+        scrollbar.grid(row=0, column=1, sticky='ns')
+        self.canvas.config(yscrollcommand=scrollbar.set)
+        print(self.canvas.bbox('all'))
+        
+        
+        # self.canvas.bind('<Configure>', self.on_configure)
 
         try :
           rank = run(self.selecteddocument.get(), self.selectedModel.get(), self.NumSerie.get())
           aux = 0
           for doc in rank:
-           
             self.ShowTitle(doc)
             aux += 1
+          
+          self.canvas.config(scrollregion=self.canvas.bbox('all'))
+          self.queryButton["state"]=DISABLED
         except Exception as e :
           raise e
         
@@ -91,13 +143,17 @@ class Google:
 
     def ShowTitle(self,doc):
         self.doc=doc
-        self.linkcol+=1
-        self.linkrow+=1
+        self.linkrow+=2
+        
         try:
-            Linkbutton(self.window, text= doc["title"], command= lambda m=doc: self.OpenWindowDoc(m)).grid(row=self.linkrow,column=0)
+            Linkbutton(self.canvas, text= doc["title"], command= lambda m=doc: self.OpenWindowDoc(m)).grid(row=self.linkrow,column=0)
+            Label(self.canvas, text= doc["body"]).grid(row=self.linkrow+1,column=0)
         except:
-            Linkbutton(self.window, text= doc["name"], command= lambda m=doc: self.OpenWindowDoc(m)).grid(row=self.linkrow,column=0)
-        # docButton.grid(row=self.linkrow,column=0)
+            Linkbutton(self.canvas, text= doc["name"], command= lambda m=doc: self.OpenWindowDoc(m)).grid(row=self.linkrow,column=0)
+            Label(self.canvas,text = doc["body"],wraplength=1000,background="lightblue").grid(row=self.linkrow+1,column=0)
+          
+        
+
     def OpenWindowDoc(self,doc):
         newwindow =Toplevel(self.window,background="white")
         mensaje = Text(newwindow, background="white", width=165, height=25)
@@ -105,7 +161,18 @@ class Google:
         mensaje.insert("insert",doc["body"])
 
         newwindow.mainloop()
+    
+    def OpenWindowHelp(self,textHelp):
+        newwindow =Toplevel(self.window,background="white")
+        mensaje = Text(newwindow, background="white", width=165, height=25)
+        mensaje.grid(column=0,row=0,padx=0, pady=125)
+        mensaje.insert("insert",textHelp)
 
+        newwindow.mainloop()
+
+    def CloseFrame(self):
+        self.lf.destroy()
+        self.queryButton['state'] = NORMAL
 
     def MessageForModel(self, event):
         if self.selectedModel.get() == "BooleanModel":
@@ -113,8 +180,6 @@ class Google:
                 title="Query info",
                 message="For this model use boolean lenguage in query(Example: a & b, a | b, a and/or b, ~a & b)",
             )
-    def callback(url):
-        webbrowser.open_new(url)
 
 listA = ["BooleanModel", "VectorialModel", "LSIModel"]
 listD = ["Prueba","cranfield", "20NewGroups"]
