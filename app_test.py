a576028b09090b49d8cfc1cf5f6c415a05232820
jorgesolerrr <jorgeasoler2015@gmail.com>
2022-12-21 12:41:02 -0500

    final changes

--- app_test.py
+++ app_test.py
@@ -1,24 +1,8 @@
 from tkinter import Tk
 from frontend import Google
 from backend import run, set_parser
-from sympy import to_dnf
 
 
-
-
-# a = [2, 3, 4]
-# b = [1,5,2]
-# result = set(b).union(a)
-# print(result)
-
-# from boolean import BooleanAlgebra
-
-
-# aquí especificar el dataset con el que se va  a trabajar("Prueba", "cranfield", "20NewGroups")
-# alguno de los que están entre paréntesis
-
-
-# aquí escribir la query
 if __name__ == "__main__":
     root = Tk()
     menu = Google(root)
